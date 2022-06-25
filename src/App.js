c1256827da29d9a21ce67b4cf0c3d3fa675362af
benitezfernando <benitezfernando87@gmail.com>
2022-06-25 16:20:54 -0300

    viaje a mexico

--- src/App.js
+++ src/App.js
@@ -41,7 +41,6 @@ function App() {
 
   return (
     <Fragment>
-      <p className='titulo'>👨‍🌾</p>
       <div className="container">
         <div className="row">
           <div className="one-half column">
